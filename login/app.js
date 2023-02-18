a78786037eab2d8b543e30bd0ee70da9c70079da
TunpisitSomket <86254542+TunpisitSomket@users.noreply.github.com>
2023-02-18 21:30:14 +0700

    non_login

--- login/app.js
+++ login/app.js
@@ -11,3 +11,11 @@ app.use(express.urlencoded({extended: false}));
 app.set('views' , path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cookieSession({
+    name: 'session',
+    keys: ['key1', 'key2'],
+    maxAge: 3600 * 1000  // 1 hours
+}));
+
+app.listen(3000, () => console.log('Server Online'));
+
